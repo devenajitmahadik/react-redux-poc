185123511579a0d978b79ee29c1c864177f9d5ba
devenajitmahadik <31102748+devenajitmahadik@users.noreply.github.com>
2017-08-29 01:41:23 +0530

    Add file

--- my-app/client/index.js
+++ my-app/client/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import VacancyAnnouncer from './components/VacancyAnnouncer.jsx';
+import Board from './components/Board.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div><App /></div>, document.getElementById('root'));
+ReactDOM.render(<VacancyAnnouncer vacancycount={0}/>, document.getElementById('show-vacancy'));
+// ReactDOM.render(<div>
+// 					<Comments>It is comment using the children prop</Comments>
+// 					<Comments comment="This comment using comment prop"/>
+// 					<Comments />
+// 				</div>,document.getElementById('comments-container'));
+
+ReactDOM.render(<Board />,document.getElementById('comments-container'));
